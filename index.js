b463c0e5e8fbd95abca4ce648327eeb59fc32fc4
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2020-05-29 13:40:19 -0400

    update

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express');
-
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20')
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send({ bye: 'buddy' });
-});
+passport.use(new GoogleStrategy());
+
+// client ID 758725786283-ek0a6t676ic0esubh0q64uh9tqp5g8jv.apps.googleusercontent.com
+// Key LBH2r1ga9ZEVpANFcu-SisZp
+
+// app.get('/', (req, res) => {
+//     res.send({ bye: 'buddy' });
+// });
 
 const PORT = process.env.PORT || 5000;
 
