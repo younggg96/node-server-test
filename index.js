c84c09cdaf670a54bf57daf0064f589db7b982b2
younggg96 <yangguannggeng960123@gmail.com>
2020-05-30 12:05:23 -0400

    new update

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20')
+const keys = require('./config/key')
 const app = express();
 
-passport.use(new GoogleStrategy());
+passport.use(new GoogleStrategy(
+    {
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+        console.log(accessToken)
+        console.log(refreshToken)
+        console.log(profile)
+        console.log(" "+ done)
+    }
+
+));
+
+app.get(
+    '/auth/google',
+    passport.authenticate('google', {
+        scope: ['profile', 'email']
+    })
+)
 
 // client ID 758725786283-ek0a6t676ic0esubh0q64uh9tqp5g8jv.apps.googleusercontent.com
 // Key LBH2r1ga9ZEVpANFcu-SisZp
