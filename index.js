380f52a54b165bfe24aae220eb49733b5cf00726
Guanggenng Yang <42145357+younggg96@users.noreply.github.com>
2020-06-01 19:50:03 -0400

    add cookieSession

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const keys = require('./config/key');
-require('./models/User')
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const keys = require("./config/key");
+require("./models/User");
 require("./services/passport");
 // Mongo Installed && Mongoose Installed
 // Need to be able to identify users who sign up and return to our application.
 // We want to save the 'id' in their google profile
-mongoose.connect(keys.mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
-
-
+mongoose.connect(keys.mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 const app = express();
+app.use(
+  cookieSession({
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+    keys: [keys.cookieKey]
+  })
+);
 require("./routes/authRoutes")(app);
 
 const PORT = process.env.PORT || 5000;
